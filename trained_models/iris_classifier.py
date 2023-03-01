a807cfd32994fbf6284a46f372486a0cab54b669
dominikriedermbw <dominik.rieder@maibornwolff.de>
2023-03-01 19:22:05 +0100

    commit

--- trained_models/iris_classifier.py
+++ trained_models/iris_classifier.py
@@ -3,9 +3,12 @@ import os.path
 import numpy
 from joblib import load
 #import pandas as pd
+import pickle
 
 path = os.path.dirname(os.path.abspath(__file__))
-iris_classifier = load(f"{path}/persisted_models/trained_iris_model.joblib")
+#iris_classifier = load(f"{path}/persisted_models/trained_iris_model.joblib")
+with open(f"{path}/persisted_models/trained_iris_model.pickle", "rb") as file:
+    iris_classifier = pickle.load(file)
 
 def predict_iris_species(sepal_length, sepal_width, petal_length, petal_width) -> str:
     """to_predict = pd.DataFrame.from_dict(
