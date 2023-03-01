5cdd4ebc7db426c5710bc1799ce4a5998a21deee
dominikriedermbw <dominik.rieder@maibornwolff.de>
2023-03-01 19:02:01 +0100

    commit

--- trained_models/iris_classifier.py
+++ trained_models/iris_classifier.py
@@ -1,27 +1,24 @@
 import os.path
-from typing import List
 
 import numpy
 from joblib import load
-from sklearn.svm import SVC
-import pandas as pd
+#import pandas as pd
 
 path = os.path.dirname(os.path.abspath(__file__))
-iris_classifier: SVC = load(f"{path}/persisted_models/trained_iris_model.joblib")
+iris_classifier = load(f"{path}/persisted_models/trained_iris_model.joblib")
 
 def predict_iris_species(sepal_length, sepal_width, petal_length, petal_width) -> str:
-    to_predict = pd.DataFrame.from_dict(
+    """to_predict = pd.DataFrame.from_dict(
         data={
             "sepal_length": [sepal_length],
             "sepal_width": [sepal_width],
             "petal_length": [petal_length],
             "petal_width": [petal_width]
         }
-    )
+    )"""
+    to_predict = [
+        [sepal_length, sepal_width, petal_length, petal_width]
+    ]
 
     result: numpy.ndarray = iris_classifier.predict(to_predict)
     return result.tolist()
-
-"""print(predict_iris_species(
-    6.0, 2.0, 2.0, 0.4
-))"""
