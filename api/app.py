e69e645c1630ddd9e2267d2d6c2cda661f1d9b5b
dominikriedermbw <dominik.rieder@maibornwolff.de>
2023-03-02 14:10:52 +0100

    revert changes

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 # save this as app.py
 import os
-from flask import Flask, Response, request
+from flask import Flask, Response
+from flask import request as flask_request
 
 import sys
 sys.path.append("..")
@@ -12,19 +13,40 @@ FLASK_API_KEY = os.getenv("FLASK_API_KEY")
 app = Flask(__name__)
 
 
-@app.before_request
+"""@app.before_request
 def check_api_key():
     request_api_key = request.headers.get("api-key")
     if request_api_key != FLASK_API_KEY:
-        return Response("Invalid Api Key", 401)
+        return Response("Invalid Api Key", 401)"""
 
 @app.route("/")
-def hello():
-    return {"answer": "Hello, World!"}
+def api_info():
+    return {
+        "/predict_iris_species ": "params: sepal_length, sepal_width, petal_length, petal_width",
+        "/": "endpoint infos"
+    }
+
+@app.route("/another")
+def another():
+    return {
+        "/predict_iris_species ": "params: sepal_length, sepal_width, petal_length, petal_width",
+        "/": "endpoint infos"
+    }
 
 @app.route("/predict_iris_species")
 def predict_iris_species():
-    return iris_classifier.predict_iris_species(**request.json)
+    try:
+        #breakpoint()
+        #return iris_classifier.predict_iris_species(**request.json)
+        return flask_request.json
+    except Exception as e:
+        return f"An error occured: {str(e)}."
+    """return iris_classifier.predict_iris_species(
+        sepal_length=5.0,
+        sepal_width=5.0,
+        petal_length=5.0,
+        petal_width=5.0
+    )"""
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8080)
+    app.run(host="localhost", port=8000)
--- trained_models/iris_classifier.py
+++ trained_models/iris_classifier.py
@@ -1,32 +1,14 @@
 import os.path
 
-#from joblib import load
-#import pandas as pd
-#import pickle
 import onnxruntime as rt
-import numpy as np
 
 path = os.path.dirname(os.path.abspath(__file__))
-#iris_classifier = load(f"{path}/persisted_models/trained_iris_model.joblib")
-#with open(f"{path}/persisted_models/trained_iris_model.pickle", "rb") as file:
-#    iris_classifier = pickle.load(file)
 
-sess = rt.InferenceSession(f"{path}/persisted_models/trained_iris_model.onnx")
+iris_classifier_session = rt.InferenceSession(f"{path}/persisted_models/trained_iris_model.onnx")
 
 def predict_iris_species(sepal_length, sepal_width, petal_length, petal_width) -> str:
-    """to_predict = pd.DataFrame.from_dict(
-        data={
-            "sepal_length": [sepal_length],
-            "sepal_width": [sepal_width],
-            "petal_length": [petal_length],
-            "petal_width": [petal_width]
-        }
-    )"""
     to_predict = [
         [sepal_length, sepal_width, petal_length, petal_width]
     ]
-
-    #result = iris_classifier.predict(to_predict)
-    X = np.array([[sepal_length, sepal_width, petal_length, petal_width]], dtype=np.float32)
-    result = sess.run(None, {'input': X})
+    result = iris_classifier_session.run(None, {'input': to_predict})
     return result[0].tolist()
